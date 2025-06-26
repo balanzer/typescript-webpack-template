3a9b9201d11c2825c4e3022093ae901698ea1fb2
Murali Varath <muralitharan.balanzer@gmail.com>
2025-06-26 15:49:14 -0400

    added logger

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,9 @@
 import "./css/style.css";
-
+import { Logger } from "./scripts/logger/logger";
 import { createHeading } from "./greetings";
 
 const heading = createHeading("Hello, World!");
 document.body.appendChild(heading);
+
+const logger = new Logger("AppLogger");
+logger.info("Application started successfully.");
---
+++ src/scripts/logger/logger.ts
@@ -0,0 +1,37 @@
+interface ILogger {
+  debug(message: string, ...args: any[]): void;
+  info(message: string, ...args: any[]): void;
+  warn(message: string, ...args: any[]): void;
+  error(message: string, ...args: any[]): void;
+}
+
+export class Logger implements ILogger {
+  private name: string;
+  constructor(name: string) {
+    this.name = name;
+  }
+  private formatMessage(message: string, ...args: any[]): string {
+    return `[${this.name}] ${message} ${args
+      .map((arg) => JSON.stringify(arg))
+      .join(" ")}`;
+  }
+  debug(message: string, ...args: any[]): void {
+    const formattedMessage = this.formatMessage(message, ...args);
+    console.debug(formattedMessage);
+  }
+
+  info(message: string, ...args: any[]): void {
+    const formattedMessage = this.formatMessage(message, ...args);
+    console.info(formattedMessage);
+  }
+
+  warn(message: string, ...args: any[]): void {
+    const formattedMessage = this.formatMessage(message, ...args);
+    console.warn(formattedMessage);
+  }
+
+  error(message: string, ...args: any[]): void {
+    const formattedMessage = this.formatMessage(message, ...args);
+    console.error(formattedMessage);
+  }
+}
