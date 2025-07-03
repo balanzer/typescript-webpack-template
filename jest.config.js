348c3f9e82ac144a7c20f9162640d98476f07c71
Murali Varath <muralitharan.balanzer@gmail.com>
2025-07-03 09:53:19 -0400

    enabled code coverage for jest

--- jest.config.js
+++ jest.config.js
@@ -9,4 +9,18 @@ module.exports = {
   transform: {
     ...tsJestTransformCfg,
   },
+  collectCoverage: true,
+  collectCoverageFrom: [
+    "scripts/src/**/*.{js,jsx,ts,tsx}",
+    "!scripts/src/**/*.d.ts", // Exclude declaration files
+  ],
+  coverageReporters: ["clover", "json", "lcov", ["text", { skipFull: true }]],
+  coverageThreshold: {
+    global: {
+      statements: 80,
+      branches: 80,
+      functions: 80,
+      lines: 80,
+    },
+  },
 };
